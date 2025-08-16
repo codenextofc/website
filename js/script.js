b6c40dfda1741138cf3c2e1b3aeaf4ece1ff81a8
joaostorres2024 <joao.pedrost2004@gmail.com>
2025-08-16 12:11:39 -0300

    Javascript com erros

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
-  let currentScroll = window.scrollY;
-  let targetScroll = window.scrollY;
+let currentScroll = window.scrollY;
+let targetScroll = window.scrollY;
+let isScrollingWithWheel = false;
 
-  window.addEventListener('wheel', (e) => {
-    e.preventDefault(); 
-    targetScroll += e.deltaY; 
-  }, { passive: false });
+window.addEventListener('wheel', (e) => {
+  if (!e.ctrlKey) {
+    e.preventDefault(); // bloqueia apenas o scroll da roda do mouse
+    targetScroll += e.deltaY;
+    isScrollingWithWheel = true;
+  }
+}, { passive: false });
 
-  function smoothScroll() {
-    currentScroll += (targetScroll - currentScroll) * 0.15; 
+function smoothScroll() {
+  if (isScrollingWithWheel) {
+    currentScroll += (targetScroll - currentScroll) * 0.15;
     window.scrollTo(0, currentScroll);
-    requestAnimationFrame(smoothScroll);
+  } else {
+    // Se o usuário estiver usando outro método de scroll, atualiza os valores
+    currentScroll = window.scrollY;
+    targetScroll = window.scrollY;
   }
 
-  smoothScroll();
+  requestAnimationFrame(smoothScroll);
+}
+
+smoothScroll();
